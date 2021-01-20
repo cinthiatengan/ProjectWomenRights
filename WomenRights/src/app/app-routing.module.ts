01b824f47cf7234d5179fcc71f17ec4eb1c488d7
Cinthia <cinthia.tengan@gmail.com>
2021-01-19 23:31:02 -0300

    final update, finished project by now

--- WomenRights/src/app/app-routing.module.ts
+++ WomenRights/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { CorpoComponent } from './corpo/corpo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'corpo', pathMatch:'full'},
+
+  {path:'corpo', component:CorpoComponent},
+  {path:'contato', component:ContatoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
